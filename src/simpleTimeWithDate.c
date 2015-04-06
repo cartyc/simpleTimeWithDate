125ffaa0ad5b4433a73c5d87a16f88a4970ec7a5
Chris <chris.carty@gmail.com>
2015-04-05 22:28:10 -0400

    added setLayer() to handle text layer creation and some more comments

--- src/simpleTimeWithDate.c
+++ src/simpleTimeWithDate.c
@@ -15,16 +15,60 @@ static GFont hourFont;
 static GFont minuteFont;
 static GFont datetimeFont;
 
-//Create Window
-static void init()}{
+////////////////////////////////////////////
+/////                                 //////
+/////     Set Text Layer Details      //////
+/////                                 //////
+////////////////////////////////////////////
+
+static void update_time(){
+  //Set time structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  //Create a time buffer
+  static char hourBuffer[] = "00";
+  static char minBuffer[] = "00";
+  
+  //Write the current hours and minutes!
+  if (clock_is_24h_style() == true){
+    //Use 24 hours format
+    strftime(hourBuffer, sizeof("00"), "%H", tick_time);
+    strftime(minBuffer, sizeof("00"), "%M", tick_time);
+  } else{
+    strftime(hourBuffer, sizeof("00"), "%I", tick_time);
+    strftime(minBuffer, sizeof("00"), "%M", tick_time);
+  }
+
+  //Display this on the textlayer
+  text_layer_set_text(s_minute, minBuffer);
+  text_layer_set_text(s_hour, hourBuffer);
+}
+
+////////////////////////////////////////////
+/////                                 //////
+/////   Initialize the watchFace      //////
+/////                                 //////
+////////////////////////////////////////////
+static void init(){
 
 }
-//Destroy Window
+
+
+////////////////////////////////////////////
+/////                                 //////
+/////   DeInitialize the watchFace    //////
+/////                                 //////
+////////////////////////////////////////////
 static void deinit(){
 
 }
 
-//Run the Watchface
+////////////////////////////////////////////
+/////                                 //////
+/////   Where the magic Happens       //////
+/////                                 //////
+////////////////////////////////////////////
 int main(void){
   
   // Start running the watchFace
