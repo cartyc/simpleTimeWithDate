c9087172af0027df1248522a0a72c008622d703c
Chris <chris.carty@gmail.com>
2015-04-05 22:19:48 -0400

    added basic structure

--- src/simpleTimeWithDate.c
+++ src/simpleTimeWithDate.c
@@ -1,8 +1,39 @@
 #include <pebble.h>
 
-//Set design vars
+//Set design vars Windows, Layers
 
+//Window Layer
+static Window *mainWidow;
+
+//Text Layers
+static TextLayer *s_dateLayer;
+static TextLayer *s_hourLayer;
+static TextLayer *s_minuteLayer;
+
+//Fonts
+static GFont hourFont;
+static GFont minuteFont;
+static GFont datetimeFont;
 
 //Create Window
+static void init()}{
 
+}
 //Destroy Window
+static void deinit(){
+
+}
+
+//Run the Watchface
+int main(void){
+  
+  // Start running the watchFace
+  init();
+  
+  //Run the loop
+  app_event_loop(); 
+  
+  //Shut it down!
+  deinit();
+
+}
